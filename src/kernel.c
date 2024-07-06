65f326dd1fd1393b98592099b16691fc5a8dbdc0
kkCoder111 <90219573+kkCoder111@users.noreply.github.com>
2024-07-06 12:11:31 -0700

    First commit with actual code

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,19 @@
+#include <stdint.h>
+
+void printf(const char* str) {
+    volatile uint16_t* video_memory = (uint16_t*) 0xB8000;
+    uint16_t attribute = 0x0F00; // white on black
+    int offset = 0;
+    int vga_width = 80;
+    int vga_height = 25;
+    
+    while (*str) {
+        video_memory[offset] = (*str) | attribute;
+        str++;
+        offset++;
+    }
+}
+
 extern void main(){
-    *(char*)0xb8000 = 'Q';
-    return;
+    printf("Starting kkOS...");
 }
